34419a2d4992f05872aed52156ccdca019f9ebcf
Bilal Yaylak <bilalyaylak@gmail.com>
2022-07-12 21:10:18 +0300

    Implement 8XY6 instruction

--- src/chip8.rs
+++ src/chip8.rs
@@ -282,6 +282,13 @@ impl Chip8 {
                 self.v[0xF] = if !borrow { 0x1 } else { 0x0 };
                 self.duration_until_next_execute = Duration::from_micros(200);
             }
+            // SHR Vx {, Vy}
+            (0x8, _, _, 0x6) => {
+                let vx = self.v[x as usize];
+                self.v[x as usize] = vx >> 1;
+                self.v[0xF] = vx & 0x1;
+                self.duration_until_next_execute = Duration::from_micros(200);
+            }
             // LD I, addr
             (0xA, _, _, _) => {
                 self.i = nnn;
