a6d0d6d9b5e3ae19fc3d3189d8b0f51822891e72
Bilal Yaylak <bilalyaylak@gmail.com>
2022-07-12 21:09:44 +0300

    Implement 8XY3 instruction

--- src/chip8.rs
+++ src/chip8.rs
@@ -263,6 +263,11 @@ impl Chip8 {
                 self.v[x as usize] &= self.v[y as usize];
                 self.duration_until_next_execute = Duration::from_micros(200);
             }
+            // XOR Vx, Vy
+            (0x8, _, _, 0x3) => {
+                self.v[x as usize] ^= self.v[y as usize];
+                self.duration_until_next_execute = Duration::from_micros(200);
+            }
             // LD I, addr
             (0xA, _, _, _) => {
                 self.i = nnn;
